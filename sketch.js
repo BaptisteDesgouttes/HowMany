6dc6533a329012e6c1083de905bae2fe7c706eab
maximegi <maxime.giraudeau1@gmail.com>
2021-05-29 21:39:17 +0200

    p5 lights

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
-// let monkey;
-// let angle = 0;
+var easycam;
+let scene;
+let angle = 0;
+let tweet = [];
+let lightTweets = [];
 
-// function preload() {
-//   monkey = loadModel('monkey.obj');
-// }
+function preload() {
+  scene = loadModel('people.obj');
+  // scene = loadModel('scene.obj');
+  // scene = loadModel('sol_avec_corps-petit.obj');
+}
 
 // function setup() {
 //   createCanvas(1536, 864, WEBGL);
@@ -30,7 +35,8 @@
 var socket;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  //easycam = createEasyCam();
   background(0);
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
@@ -42,6 +48,7 @@ function setup() {
   socket.on('tweets',
     // When we receive data
     function(data) {
+      tweet = data;
       // ICI data C EST LA LISTE DES TWEETS ENVOYES
       // SI TU TE CO SUR localhost:8080 TU PEUX LES CONSOLE LOG POUR VOIR COMMENT
       // LES UTILISER. C'EST UN TABLEAU DE TWEET ET CHAQUE TWEET EST UN OBJET 
@@ -53,14 +60,85 @@ function setup() {
       // DEMANDE MOI SI TU VOIS AUCUN TWEET
     }
   );
+
+  
 }
 
 function draw() {
-  // Nothing
+  background(0);
+  
+  scale(2);    
+  translate(20, -8, 0);
+  
+  let locX = mouseX - height / 2;
+  let locY = mouseY - width / 2;
+  //pointLight(80, 80, 80, locX, locY, 10);
+  //ambientLight(255,255,255);
+  
+  for(var i = 0; i < tweet.length; i++) {
+    lightTweets[i] = new lightTweet(tweet[i].usr_verified, tweet[i].nb_fav);
+    lightTweets[i].display();
+  }
+  
+    // pointLight(90  * cos(2 * frameCount/100) + 30, 90  * cos(2 * frameCount/100) + 30, 90  * cos(2 * frameCount/100) + 30, 100, -50, -30);
+    // push();
+    //   translate(100,-50, -30);
+    //   normalMaterial();
+    //   specularMaterial(255);
+    //   sphere(1);
+    // pop();
+
+    // pointLight((255 - 204) * sin(frameCount/60) + 30, (242 - 204) * sin(frameCount/60) + 30, (204 - 204) * sin(frameCount/60) + 30, -100, 0, 200);
+    // push();
+    //   translate(-100, 0, 200);
+    //   normalMaterial();
+    //   specularMaterial(255);
+    //   sphere(1);
+    // pop();
+
+    rotateX(-PI/6);
+    rotateY(-PI/3);
+    rotateZ(PI);
+    
+    normalMaterial();
+    ambientMaterial(255);
+    model(scene);
 }
 
 // Function for sending to the socket
 function sendping() {
   // Send that object to the socket
   socket.emit('message', "Data sent");
+}
+
+function lightTweet(verified, likes) {
+  
+  this.color = color(0, 0, 0);
+  
+  this.x = random(-100,100); 
+  this.y = random(0,100);
+  this.z = random(-100,100);
+
+  if(verified) {
+    this.color.setRed((255 - 203) * map(likes, 0, 1000000, 0, 1));
+    this.color.setBlue((242 - 203) * map(likes, 0, 1000000, 0, 1));
+    this.color.setGreen((204 - 203) * map(likes, 0, 1000000, 0, 1));
+  }
+
+  else {
+    this.color.setRed((90) * map(likes, 0, 1000000, 0, 1));
+    this.color.setBlue((90) * map(likes, 0, 1000000, 0, 1));
+    this.color.setGreen((90) * map(likes, 0, 1000000, 0, 1));
+  }
+  
+  this.display = function() {
+    pointLight(this.color, this.x, this.y, this.z);
+    push();
+      translate(this.x, this.y, this.z);
+      normalMaterial();
+      specularMaterial(255);
+      sphere(1);
+    pop();
+  }
+
 }
